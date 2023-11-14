c073c5f81e02239b3fb72afbdafd6cba364a42eb
The-Gaia-Project <gabrielueland42@yahoo.com>
2023-11-14 08:36:31 +0100

    new update for my website

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ function toggleNav() {
   var sidebar = document.getElementById("mySidebar");
   var btn = document.querySelector(".toggle-btn");
 
+  // Add transition for smooth toggling
+  sidebar.style.transition = "0.5s";
+  btn.style.transition = "0.5s";
+
   if (sidebar.style.width === '250px') {
     sidebar.style.width = '0';
     btn.style.left = '0';
@@ -9,6 +13,50 @@ function toggleNav() {
   } else {
     sidebar.style.width = '250px';
     btn.style.left = '250px';
-    btn.innerHTML = '☰';
+    btn.innerHTML = '×';
+  }
+
+  // Remove transition after a delay equal to the transition duration
+  setTimeout(() => {
+    sidebar.style.transition = "none";
+    btn.style.transition = "none";
+  }, 500); // 500 milliseconds or the duration of your transition
+}
+
+
+// Additional JavaScript for dynamic sidebar positioning
+window.onscroll = function() {
+  var sidebar = document.getElementById("mySidebar");
+  var btn = document.querySelector(".toggle-btn");
+
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    sidebar.style.top = '0';
+    btn.style.top = '10px';
+  } else {
+    sidebar.style.top = '60px';
+    btn.style.top = '20px';
+  }
+};
+
+function closeAllSubmenus() {
+  var submenus = document.getElementsByClassName('submenu');
+  for (var i = 0; i < submenus.length; i++) {
+    submenus[i].style.display = 'none';
   }
 }
+window.onscroll = function() {
+  var sidebar = document.getElementById("mySidebar");
+  var btn = document.querySelector(".toggle-btn");
+  var header = document.getElementById("header");
+
+  // Calculate the bottom position of the header
+  var headerBottom = header.offsetTop + header.offsetHeight;
+
+  if (window.pageYOffset > headerBottom) {
+    sidebar.style.top = '0';
+    btn.style.top = '10px';
+  } else {
+    sidebar.style.top = (headerBottom - window.pageYOffset) + 'px';
+    btn.style.top = (headerBottom - window.pageYOffset + 10) + 'px';
+  }
+};
